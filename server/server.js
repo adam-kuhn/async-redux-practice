0fd73cfb0827f9e01d7b32ba0c56541dd96d8b9a
Adam Kuhn <adam.kuhn20@gmail.com>
2018-02-14 18:05:12 +1300

    XKCD not working, not sure where broken

--- server/server.js
+++ server/server.js
@@ -1,14 +1,16 @@
 var path = require('path')
 var express = require('express')
+var server = express()
+const cors = require('cors')
 
 var redditRoutes = require('./reddit')
 const xkcdRoutes = require('./xkcd')
-var server = express()
 
 server.use(express.static(path.join(__dirname, '../public')))
+server.use(cors({origin: 'http://localhost:8080'}))
 
 server.use('/api/v1/reddit', redditRoutes)
-// server.use('/api/v1/xkcd', xkcdRoutes)
+server.use('/api/v1/xkcd', xkcdRoutes)
 // server.use('/api/v1/xkcd', xkcdRoutes)
 
 module.exports = server
--- server/xkcd.js
+++ server/xkcd.js
@@ -20,3 +20,4 @@ router.get('/comic/:comic', (req, res) => {
     )
 })
 
+module.exports = router
